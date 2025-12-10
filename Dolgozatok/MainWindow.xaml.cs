a4481b638f898cd57ad1dad18bf113c7234f8514
László Tamás <laszlot@kkszki.hu>
2025-12-10 11:28:35 +0100

    2cs

--- Dolgozatok/MainWindow.xaml.cs
+++ Dolgozatok/MainWindow.xaml.cs
@@ -31,5 +31,22 @@ namespace WpfDolgozat
             dgAdatok.ItemsSource = dolgozatok;
         }
 
+        private string FindProjectFile(string fileName)
+        {
+            string dir = AppDomain.CurrentDomain.BaseDirectory;
+
+            // Legfeljebb 10 szinttel megyünk feljebb → biztosan elérjük a projekt gyökerét
+            for (int i = 0; i < 10; i++)
+            {
+                string possibleFile = Path.Combine(dir, fileName);
+                if (File.Exists(possibleFile))
+                    return possibleFile;
+
+                dir = Directory.GetParent(dir).FullName;
+            }
+
+            return null;
+        }
+
     }
 }
