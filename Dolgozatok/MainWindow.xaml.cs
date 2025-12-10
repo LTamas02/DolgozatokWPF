5269852b6d212e07dc27f3bf3699efd998947973
László Tamás <laszlot@kkszki.hu>
2025-12-10 11:28:10 +0100

    1cs

--- Dolgozatok/MainWindow.xaml.cs
+++ Dolgozatok/MainWindow.xaml.cs
@@ -1,24 +1,35 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Dolgozatok
+namespace WpfDolgozat
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Dolgozat> dolgozatok =
+            new ObservableCollection<Dolgozat>();
+
+        private string filePath;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            filePath = FindProjectFile("dolgozatok.txt");
+
+            if (filePath == null)
+            {
+                MessageBox.Show("Nem található a dolgozatok.txt a projektben!");
+                return;
+            }
+
+            MessageBox.Show("Használt fájl:\n" + filePath);
+
+            Beolvas();
+            dgAdatok.ItemsSource = dolgozatok;
         }
+
     }
 }
